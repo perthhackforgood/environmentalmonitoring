9d38ce3a34c4ec2fd8669450ed90317960f9fc0e
sanjay gonsalves <emailkarsans-subscriptions@yahoo.com.au>
2019-11-30 14:17:33 +0800

    moved to dotnet 2.1

---
+++ api/phfg-apiv2/UploadPhotoHttp/UploadPhotoHttp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace phfg_apiv2.api.UploadPhotoHttp
+{
+    public static class UploadPhotoHttp
+    {
+        [FunctionName("UploadPhoto")]
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]
+            HttpRequestMessage req,
+            TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+            PhotoDetails photo = (PhotoDetails)await req.Content.ReadAsAsync<Object>();
+
+            return photo == null
+                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
+                : req.CreateResponse(HttpStatusCode.OK, "Hello " + photo.DeviceId);
+        }
+    }
+
+    public class PhotoDetails
+    {
+        public string ImageAsBase64Str { get; set; }
+        public string DeviceId { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string TimeOfPhotoTaken { get; set; }
+        public double TempOfPhotoTaken { get; set; }
+        public string CompassDirectionOfDevice { get; set; }
+        public string AddressFromReverseGeoCoding { get; set; }
+    }
+}
---
+++ api/phfg-apiv2/UploadedPhotoTrigger/run.csx
@@ -0,0 +1,4 @@
+﻿public static void Run(Stream myBlob, string name, ILogger log)
+{
+    log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+}
