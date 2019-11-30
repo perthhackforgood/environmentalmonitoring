8113895fedffb868cbd2568176dc2efe0de125ab
tdeboer <tim.deboer@interfuze.com.au>
2019-11-30 14:07:57 +0800

    UploadPhoto first cut. Should just return the mobile identifier for good request

--- api/phfg.api/UploadPhotoHttp/UploadPhotoHttp.cs
+++ api/phfg.api/UploadPhotoHttp/UploadPhotoHttp.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -11,25 +12,29 @@ namespace phfg.api.UploadPhotoHttp
     public static class UploadPhotoHttp
     {
         [FunctionName("UploadPhoto")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]
+            HttpRequestMessage req, 
+            TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
+            PhotoDetails photo = (PhotoDetails)await req.Content.ReadAsAsync<Object>();
 
-            // parse query parameter
-            string name = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
-                .Value;
-
-            if (name == null)
-            {
-                // Get request body
-                dynamic data = await req.Content.ReadAsAsync<object>();
-                name = data?.name;
-            }
-
-            return name == null
+            return photo == null
                 ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+                : req.CreateResponse(HttpStatusCode.OK, "Hello " + photo.DeviceId);
         }
+    }
+
+    public class PhotoDetails
+    {
+        public string ImageAsBase64Str { get; set; }
+        public string DeviceId { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string TimeOfPhotoTaken { get; set; }
+        public double TempOfPhotoTaken { get; set; }
+        public string CompassDirectionOfDevice { get; set; }
+        public string AddressFromReverseGeoCoding { get; set; }
     }
 }
