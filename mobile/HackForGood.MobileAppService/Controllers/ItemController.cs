207d5ed061e31ba2ab0b3bf7ab4fb7732c89f1b7
Zac <zac@progressionem.com.au>
2019-11-30 14:29:40 +0800

    feature/Mobile Backend Service #2

--- mobile/HackForGood.MobileAppService/Controllers/ItemController.cs
+++ mobile/HackForGood.MobileAppService/Controllers/ItemController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using HackForGood.Models;
+using HackForGood.MobileAppService.Models;
 
 namespace HackForGood.Controllers
 {
@@ -20,7 +21,7 @@ namespace HackForGood.Controllers
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public ActionResult<IEnumerable<Item>> List()
+        public ActionResult<IEnumerable<Photo>> List()
         {
             return ItemRepository.GetAll().ToList();
         }
@@ -28,9 +29,9 @@ namespace HackForGood.Controllers
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult<Item> GetItem(string id)
+        public ActionResult<Photo> GetItem(string id)
         {
-            Item item = ItemRepository.Get(id);
+            Photo item = ItemRepository.Get(id);
 
             if (item == null)
                 return NotFound();
@@ -41,16 +42,16 @@ namespace HackForGood.Controllers
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public ActionResult<Item> Create([FromBody]Item item)
+        public ActionResult<Photo> Create([FromBody]Photo item)
         {
-            ItemRepository.Add(item);
+            ItemRepository.Publish(item);
             return CreatedAtAction(nameof(GetItem), new { item.Id }, item);
         }
 
         [HttpPut]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public ActionResult Edit([FromBody] Item item)
+        public ActionResult Edit([FromBody] Photo item)
         {
             try
             {
@@ -68,7 +69,7 @@ namespace HackForGood.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult Delete(string id)
         {
-            Item item = ItemRepository.Remove(id);
+            Photo item = ItemRepository.Remove(id);
 
             if (item == null)
                 return NotFound();
