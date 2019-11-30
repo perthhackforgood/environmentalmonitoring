28817e6aa5fc26d1dc2264093d9c407d04bdb76a
Dave A-W <david.allen-williams@decipher.com.au>
2019-11-30 15:26:42 +0800

    Add Cosmos nugets, dummy function

--- api/phfg-apiv2/TestCosmos/DummyAddTableItem.cs
+++ api/phfg-apiv2/TestCosmos/DummyAddTableItem.cs
@@ -1,33 +1,27 @@
 using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace phfg_apiv2.TestCosmos
 {
     public static class DummyAddTableItem
     {
         [FunctionName("DummyAddTableItem")]
-        public static async Task<IActionResult> Run(
+        public static void Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDB(
+                databaseName: "ToDoItems",
+                collectionName: "Items",
+                ConnectionStringSetting = "CosmosDBConnection")]out dynamic document,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
+            document = new { Description = "Test", id = Guid.NewGuid() };
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            log.LogInformation($"C# Queue trigger function inserted one row");
+            log.LogInformation($"Description=Test");
         }
     }
 }
