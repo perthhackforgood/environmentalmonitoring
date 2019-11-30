8040415188e3f196856efd0f326478185985d1a1
sanjay gonsalves <emailkarsans-subscriptions@yahoo.com.au>
2019-11-30 14:27:30 +0800

    Added a health check :)

---
+++ api/phfg-apiv2/HealthCheck.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace phfg_apiv2
+{
+    public static class HealthCheck
+    {
+        [FunctionName("healthCheck")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            return (ActionResult)new OkObjectResult($"Hello World");
+        }
+    }
+}
